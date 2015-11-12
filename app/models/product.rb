20aff476c110cab4a1aa53ffb2fcb8b4c75f7d1f
stephthi <nstephanie2@gmail.com>
2015-11-12 17:09:34 -0500

    Set home to login page

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,3 +10,4 @@ class Product < ActiveRecord::Base
   	sprintf("%.2f", price_in_dollars)
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'sessions#new'
   # get 'reviews/show'
 
   # get 'reviews/create'
@@ -15,7 +16,7 @@ Rails.application.routes.draw do
 
   # get 'users/create'
 
-  # resources :products
+  resources :products
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
